26e22366fbddb4eb1f769364f946f76d37932844
Etfagha oubeid Elatigh <etfaghaoubeid@gmail.com>
2021-07-10 16:42:36 +0000

    Code review
    review appartement route

--- routers/apartment.router.js
+++ routers/apartment.router.js
@@ -1,7 +1,7 @@
 const { Router } = require('express')
 const { setApartment } = require('../controllers/apartment.controller')
 const router = Router()
-
+// you should create routes for all posible operations you will perform on Apparent  but this is a good start 
 router.post('/', setApartment)
 
 module.exports = router
